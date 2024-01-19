78a59dd1dd38063ca57c051b359993e1c2f48298
Romain Bioteau <romain.bioteau@bonitasoft.com>
2024-01-19 16:14:39 +0100

    feat(upload): Retrive an existing folder if any (#8)

--- gdrive-core/src/main/kotlin/org/bonitasoft/gdrive/core/GDriveTask.kt
+++ gdrive-core/src/main/kotlin/org/bonitasoft/gdrive/core/GDriveTask.kt
@@ -9,6 +9,7 @@ import com.google.api.services.drive.DriveScopes
 import com.google.api.services.drive.model.File
 import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.GoogleCredentials
+import java.security.MessageDigest
 
 abstract class GDriveTask(private val logger: Logger, private val googleCredentials: String) {
 
@@ -45,16 +46,56 @@ abstract class GDriveTask(private val logger: Logger, private val googleCredenti
             mimeType = FOLDER_MIMETYPE
             parents = mutableListOf(parentFolderId)
         }
+        // Check if the folder already exists
+        val query = "mimeType='$FOLDER_MIMETYPE' and name='$folderName' and '$parentFolderId' in parents and trashed=false";
+        val result = drive.files().list().setQ(query)
+                        .setSupportsAllDrives(true)
+                        .setIncludeItemsFromAllDrives(true)
+                        .execute()
+        if(!result.getFiles().isEmpty()){
+            return result.getFiles().get(0)
+        }
         folder = drive.files().create(folder).setFields("id").setSupportsAllDrives(true).execute()
         return folder
     }
 
     protected fun uploadFile(drive: Drive, file: java.io.File, fileName: String, destinationFolder: File): File {
-        return drive.files().create(File().apply {
-            name = fileName
-            parents = mutableListOf(destinationFolder.id)
-        }, FileContent(null, file))
-                .setFields("id").setSupportsAllDrives(true).execute()
+         // Check if the file already exists
+         val query = "name='$fileName' and '${destinationFolder.id}' in parents and trashed=false";
+         val result = drive.files().list().setQ(query)
+                         .setSupportsAllDrives(true)
+                         .setIncludeItemsFromAllDrives(true)
+                         .execute()
+         if(!result.getFiles().isEmpty()){ // Update it
+            val existingFile = result.getFiles().get(0)
+            val uploadedFile = drive.files().update(existingFile.id, File().apply {
+                name = fileName
+            }, FileContent(null, file))
+                    .setFields("id,name,md5Checksum").setSupportsAllDrives(true).execute()
+            validateMd5Checksum(uploadedFile.md5Checksum, calculateMd5Checksum(file))
+            return uploadedFile
+         }else{ // Create it
+            val uploadedFile = drive.files().create(File().apply {
+                name = fileName
+                parents = mutableListOf(destinationFolder.id)
+            }, FileContent(null, file))
+                    .setFields("id,name,md5Checksum").setSupportsAllDrives(true).execute()
+            validateMd5Checksum(uploadedFile.md5Checksum, calculateMd5Checksum(file))
+            return uploadedFile
+         }
+    }
+
+    fun validateMd5Checksum(uploadedFileChecksum : String, originalFileChecksum : String){
+        if(uploadedFileChecksum != originalFileChecksum){
+            throw RuntimeException("Uploaded file MD5 checksum does not match !");
+        }
+    }
+
+    fun calculateMd5Checksum(file: java.io.File): String {
+        val bytes = file.readBytes()
+        val digest = MessageDigest.getInstance("MD5")
+        val hashBytes = digest.digest(bytes)
+        return hashBytes.joinToString("") { "%02x".format(it) }
     }
 
 }
--- gdrive-core/src/main/kotlin/org/bonitasoft/gdrive/core/GDriveUploadTask.kt
+++ gdrive-core/src/main/kotlin/org/bonitasoft/gdrive/core/GDriveUploadTask.kt
@@ -2,25 +2,29 @@ package org.bonitasoft.gdrive.core
 
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.model.File
+import java.security.MessageDigest
+import java.io.BufferedWriter
+import java.io.FileWriter
 
 class GDriveUploadTask(googleCredentials: String,
 					   private val source: String,
 					   private val destinationId: String,
 					   private val logger: Logger,
-					   private val renameTo: String) : GDriveTask(logger, googleCredentials) {
+					   private val renameTo: String,
+					   private val uploadChecksum: Boolean) : GDriveTask(logger, googleCredentials) {
 
 	override fun doExecute(drive: Drive) {
 
 		val destinationFolder = retrieveFolder(drive, destinationId)
 
 		logger.info("Will upload $source inside Drive folder ${destinationFolder.name} with id ${destinationFolder.id}")
-		copy(drive, java.io.File(source), destinationFolder, renameTo)
+		copy(drive, java.io.File(source), destinationFolder, renameTo, uploadChecksum)
 		logger.info("Uploaded all files to ${destinationFolder.id}")
 
 		return
 	}
 
-	private fun copy(drive: Drive, file: java.io.File, destinationFolder: File, renameTo: String = "") {
+	private fun copy(drive: Drive, file: java.io.File, destinationFolder: File, renameTo: String = "", uploadChecksum: Boolean) {
 		logger.debug("Processing ${file.absolutePath}")
 		val fileName = renameTo.trim().ifEmpty { file.name }
 		if (fileName != file.name) {
@@ -33,11 +37,36 @@ class GDriveUploadTask(googleCredentials: String,
 			val newFolder = createFolder(drive, fileName, destinationFolder.id)
 			logger.debug("Created folder $fileName with id ${newFolder.id}")
 			file.listFiles().forEach { child ->
-				copy(drive, child, newFolder)
+				copy(drive, child, newFolder, "", uploadChecksum)
 			}
 		} else {
 			val uploadFile = uploadFile(drive, file, fileName, destinationFolder)
 			logger.info("Uploaded file ${uploadFile.name} with id ${uploadFile.id}")
+			if(uploadChecksum){
+				val checksumFile = java.io.File.createTempFile(fileName, ".sha256")
+				writeTextToFile(calculateSHA256(file), checksumFile.absolutePath)
+				val uploadedCheckumFile = uploadFile(drive, checksumFile, fileName + ".sha256", destinationFolder)
+				logger.info("Uploaded file ${uploadedCheckumFile.name} with id ${uploadedCheckumFile.id}")
+			}
+		}
+	}
+
+	fun calculateSHA256(file: java.io.File): String {
+		val bytes = file.readBytes()
+		val digest = MessageDigest.getInstance("SHA-256")
+		val hashBytes = digest.digest(bytes)
+		return hashBytes.joinToString("") { "%02x".format(it) }
+	}
+
+	fun writeTextToFile(text: String, filePath: String) {
+		val file = java.io.File(filePath)
+	
+		try {
+			BufferedWriter(FileWriter(file)).use { writer ->
+				writer.write(text)
+			}
+		} catch (e: Exception) {
+			logger.error("An error occurred while writing to the file: $e")
 		}
 	}
 
